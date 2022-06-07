2ffb94044a37ee0473997616d78741e1b053049c
l0ki <thomas.soupizet@student-cs.fr>
2022-06-07 15:32:00 +0200

    Create genre and movie models

---
+++ backend/models/movie.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const MovieSchema = new mongoose.Schema({
+  _id: Number,
+  backdrop: String,
+  poster: String,
+  genres: [Number],
+  title: String,
+  overview: String,
+  release_date: { type: Date, default: Date.now },
+  popularity: Number,
+});
+
+const MovieModel = mongoose.model("MovieModel", MovieSchema, "movies");
+
+module.exports = MovieModel;
