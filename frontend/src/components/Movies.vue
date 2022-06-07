31c7796fd5bf1f0f87c9f1076adaaf5b4f1e5c43
Lisa Lupi <lisa.lupi@student-cs.fr>
2022-06-07 16:24:56 +0200

    modif homepage

---
+++ frontend/src/components/Movies.vue
@@ -0,0 +1,144 @@
+
+<template>
+
+  <div class="movies">
+        <div class="img__wrap">
+          <img
+            class="img_img"
+            :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`"
+          />
+          <div class="img__description_layer">
+            <p class="img__description">
+              <font size="+3">
+                <b><router-link class = "poster" to="/poster">{{ movie.title }}</router-link> </b>
+              </font>
+              <br />
+              <p class="img__description line_limit">
+                        <router-link class = "img__description" to="/poster">{{ movie.overview }}</router-link> </p>
+              <p class = "vote"> <b>Note :</b> {{ movie.vote_average }}/10</p>
+            </p>
+          </div>
+        </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Movies",
+  props: {movie:Object,},
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.movies {
+  text-align: center;
+  color : white;
+  margin: 5px;
+  margin-bottom:20px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #f15ad3;
+  font-style: normal;
+  text-decoration: none;
+}
+a:hover {
+  color: #ffbc5e;
+  transition: 0.2s;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+html {
+  font-family: helvetica, arial, sans-serif;
+}
+
+/* Présentation des films */ 
+.img__wrap {
+  position: relative;
+  height: 450px;
+  width: 300px;
+}
+
+.img__description_layer {
+  position: absolute;
+  top: 0;
+  font-size: medium;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: black;
+  color: #fff;
+  visibility: hidden;
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: opacity 0.2s, visibility 0.2s;
+}
+
+.img__description.line_limit {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+}
+.img__wrap:hover .img__description_layer {
+  visibility: visible;
+  opacity: 0.7;
+}
+
+.img__description {
+  transition: 0.2s;
+  transform: translateY(1em);
+  text-decoration: none;
+  color:white;
+}
+
+.img__wrap:hover .img__description {
+  transform: translateY(0);
+  color:white;
+
+}
+
+/* lien vers fiche des films */
+.poster{
+  font-size:larger;
+  color: white;
+  text-decoration: none;
+}
+
+.poster:hover{
+  color:#ffbc5e;
+  opacity: 1;
+}
+
+/* notation */
+.vote {
+  text-align:center;
+  font-size:medium;
+  margin-top:10px;
+}
+</style>
---
+++ frontend/src/components/Searchbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="searchbar"></div>
+  <input />
+</template>
+
+<script>
+export default {
+  name: "Searchbar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+.searchbar {
+  text-align: center;
+  color: white;
+  font-size: x-large;
+}
+
+input {
+  color: black;
+  font-size: medium;
+}
+
+input:hover {
+  font-size: larger;
+
+  transition: 0.2s;
+}
+
+input:after {
+  font-size: medium;
+  transition: 0.2s;
+}
+input:hover:after {
+  font-size: medium;
+  transition: transform 0.25s ease-out;
+}
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+html {
+  font-family: helvetica, arial, sans-serif;
+}
+</style>
